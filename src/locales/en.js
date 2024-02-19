a20b37675de570f612e68f90ed5f63c8abdb3aa2
Gabriel Rejtman <rejtmangabriel@gmail.com>
2024-02-19 01:31:24 -0400

    Primeiro Commit

---
+++ src/locales/en.js
@@ -0,0 +1,91 @@
+export default {
+  labels: {
+    monthsLong: [
+      'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ],
+    monthsShortLower: [
+      'jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'
+    ],
+    monthsLongLower: [
+      'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
+    ],
+    monthsShort: [
+      'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+    ],
+    weekdaysLong: [
+      'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'
+    ],
+    weekdaysShort: [
+      'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'
+    ],
+    weekdaysNarrow: [
+      'D', 'S', 'T', 'Q', 'Q', 'S', 'S'
+    ],
+
+  },
+
+  /*
+    - the following are colors for calendar categories
+  */
+  colors: {
+    red: {
+      1: '#DF2A79',
+      2: '#A51955',
+      3: '#73113C',
+      4: '#C20000',
+      5: '#9D0000',
+      6: '#690000',
+      7: '#460000',
+    },
+
+    orange: {
+      1: '#EE756A',
+      2: '#E84334',
+      3: '#E77C2B',
+      4: '#D56A18',
+      5: '#954A11',
+      6: '#6B350C',
+      7: '#402007',
+    },
+
+    green: {
+      1: '#1E9E98',
+      2: '#1A8A84',
+      3: '#167671',
+      4: '#115855',
+      5: '#0F4E4B',
+      6: '#09312F',
+      7: '#072726',
+    },
+
+    blue: {
+      1: '#14489A',
+      2: '#125ab8',
+      3: '#5578D7',
+      4: '#2C52BA',
+      5: '#234295',
+      6: '#1B3375',
+      7: '#142555',
+    },
+
+    purple: {
+      1: '#42338B',
+      2: '#513FAB',
+      3: '#614E7A',
+      4: '#604793',
+      5: '#49356F',
+      6: '#342650',
+      7: '#271D3C',
+    },
+
+    grey: {
+      1: '#858F93',
+      2: '#4D5458',
+      3: '#272B2C',
+      4: '#202B31',
+      5: '#2C3C44',
+      6: '#384C56',
+      7: '#516C7B',
+    },
+  },
+};
